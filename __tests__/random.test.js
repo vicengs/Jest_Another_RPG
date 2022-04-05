eb06aac996473ca200b31dd05e543b6b6762b409
Vicente Garcia <vicen1@hotmail.com>
2022-04-05 16:33:50 -0500

    Add potion mock and a player constructor

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -3,9 +3,7 @@
 test('checks if 10 is equal to 10', () => {
   expect(checkIfEqual(10, 10)).toBe(true);
 });*/
-
 const randomNumber = require('../lib/random.js');
-
 test('gets random number between 1 and 10', () => {
   expect(randomNumber()).toBeGreaterThanOrEqual(1);
   expect(randomNumber()).toBeLessThanOrEqual(10);
